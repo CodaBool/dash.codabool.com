5b9e8b3fa381833102c5558fda205e8663c9ce3e
Coda Bool <codabool@pm.me>
2024-07-14 14:50:28 -0400

    add option for using local

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-export const revalidate = 3600 // seconds until page revalidation
+// export const revalidate = 3600 // seconds until page revalidation
 
 import Link from 'next/link'
 import Image from 'next/image'
@@ -24,25 +24,50 @@ if (hour > 15 & hour <= 18) emoji = "🌤️"
 if (hour > 18 & hour <= 21) emoji = "🌄"
 if (hour > 21 & hour <= 24) emoji = "🌙"
 
-const sites = {
-  'https://media.codabool.com': '/jellyfin.webp',
-  'https://sync.codabool.com': '/sync.webp',
-  'https://movies.codabool.com': '/radarr.webp',
-  'https://tv.codabool.com': '/sonarr.webp',
-  'https://qbit.codabool.com': '/qbit.webp',
-  'https://index.codabool.com': '/jackett.webp',
-  'https://lancer.codabool.com': '/lancer.webp',
-  'https://ssh.codabool.com': '/ssh.webp',
-  'https://rss.codabool.com': '/rss.webp',
-  'https://bin.codabool.com': '/bin.webp',
-  'https://doc.codabool.com': '/doc.webp',
-  'https://rpg.codabool.com': '/d20.webp',
-  'https://plex.codabool.com': '/plex.webp',
-  'https://photos.codabool.com': '/immich.webp',
-  'https://money.codabool.com': '/money.webp',
-}
+export default function Main({ searchParams }) {
+  const { l } = searchParams
+  const useLocalIp = typeof l === "string"
+  let sites
+  if (useLocalIp) {
+    sites = {
+      'http://192.168.0.25:8096': '/jellyfin.webp',
+      'http://192.168.0.25:8384': '/sync.webp',
+      'http://192.168.0.25:7878': '/radarr.webp',
+      'http://192.168.0.25:8989': '/sonarr.webp',
+      'http://192.168.0.25:8080': '/qbit.webp',
+      'http://192.168.0.25:9696': '/jackett.webp',
+      'http://192.168.0.25:30001': '/lancer.webp',
+      'https://ssh.codabool.com': '/ssh.webp',
+      'http://192.168.0.25:8085': '/rss.webp',
+      'http://192.168.0.25:8081': '/bin.webp',
+      'http://192.168.0.25:7075': '/doc.webp',
+      'http://192.168.0.25:30000': '/d20.webp',
+      'http://192.168.0.25:32400': '/plex.webp',
+      'http://192.168.0.25:2283': '/immich.webp',
+      'https://money.codabool.com': '/money.webp',
+      'http://192.168.0.25:3001': '/uptime.webp',
+    }
+  } else {
+    sites = {
+      'https://media.codabool.com': '/jellyfin.webp',
+      'https://sync.codabool.com': '/sync.webp',
+      'https://movies.codabool.com': '/radarr.webp',
+      'https://tv.codabool.com': '/sonarr.webp',
+      'https://qbit.codabool.com': '/qbit.webp',
+      'https://index.codabool.com': '/jackett.webp',
+      'https://lancer.codabool.com': '/lancer.webp',
+      'https://ssh.codabool.com': '/ssh.webp',
+      'https://rss.codabool.com': '/rss.webp',
+      'https://bin.codabool.com': '/bin.webp',
+      'https://doc.codabool.com': '/doc.webp',
+      'https://rpg.codabool.com': '/d20.webp',
+      'https://plex.codabool.com': '/plex.webp',
+      'https://photos.codabool.com': '/immich.webp',
+      'https://money.codabool.com': '/money.webp',
+      'https://uptime.codabool.com': '/uptime.webp',
+    }
+  }
 
-export default function Main() {
   return (
     <>
       <Image
@@ -50,14 +75,18 @@ export default function Main() {
         alt="background"
         sizes="100vw"
         fill
+        priority
         style={{
           objectFit: 'cover',
           zIndex: -1
         }}
       />
-      <h1 className="time">
-        {emoji}
-      </h1>
+
+      <Link href={`${useLocalIp ? "/" : "/?l"}`} style={{ textDecoration: "none" }}>
+        <h1 className={`time ${useLocalIp ? "bounce" : ""}`}>
+          {emoji}
+        </h1>
+      </Link>
       <div className="flex-container">
         {Object.entries(sites).map(obj => (
           <div className="flex-item" key={obj[0]}>
@@ -67,11 +96,14 @@ export default function Main() {
                 src={obj[1]}
                 alt={obj[1]}
                 quality={20}
+                priority
                 sizes="(min-width: 960px) 250px, (max-width: 960px) 150px, (max-width: 544px) 100px"
                 fill
                 style={{ objectFit: 'cover' }}
               />
+              <span style={{ display: "none" }}>{obj[0]}</span>
             </Link>
+
           </div>
         ))}
       </div>
